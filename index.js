06153fe67988c08b14a0838b39c5ef63204d091d
shagyeeen <shagyeeen@gmail.com>
2026-01-12 23:35:18 +0530

    StrangR backend initial deploy 2

--- index.js
+++ index.js
@@ -13,27 +13,45 @@ const io = new Server(server, {
 
 let waitingUser = null;
 
+/* ================= HELPERS ================= */
+
+function leaveRoom(socket) {
+  if (socket.room) {
+    socket.leave(socket.room);
+    socket.room = null;
+  }
+}
+
+function pairUsers(socket, other) {
+  const room = `${socket.id}#${other.id}`;
+
+  socket.join(room);
+  other.join(room);
+
+  socket.room = room;
+  other.room = room;
+
+  io.to(room).emit("message", {
+    username: "StrangR",
+    msg: "You are now chatting with a stranger"
+  });
+}
+
+/* ================= SOCKET LOGIC ================= */
+
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
+  /* -------- JOIN -------- */
   socket.on("join", () => {
     console.log("Join request from:", socket.id);
 
+    leaveRoom(socket);
+
     if (waitingUser && waitingUser.id !== socket.id) {
-      const room = `${socket.id}#${waitingUser.id}`;
-
-      socket.join(room);
-      waitingUser.join(room);
-
-      socket.room = room;
-      waitingUser.room = room;
-
-      io.to(room).emit("message", {
-        username: "StrangR",
-        msg: "You are now chatting with a stranger"
-      });
-
+      const other = waitingUser;
       waitingUser = null;
+      pairUsers(socket, other);
     } else {
       waitingUser = socket;
       socket.emit("message", {
@@ -43,17 +61,49 @@ io.on("connection", (socket) => {
     }
   });
 
+  /* -------- NEXT STRANGER -------- */
+  socket.on("next", () => {
+    console.log("Next requested by:", socket.id);
+
+    leaveRoom(socket);
+
+    if (waitingUser === socket) {
+      waitingUser = null;
+    }
+
+    if (waitingUser) {
+      const other = waitingUser;
+      waitingUser = null;
+      pairUsers(socket, other);
+    } else {
+      waitingUser = socket;
+      socket.emit("message", {
+        username: "StrangR",
+        msg: "Waiting for a stranger..."
+      });
+    }
+  });
+
+  /* -------- MESSAGE -------- */
   socket.on("message", (data) => {
     if (socket.room) {
       io.to(socket.room).emit("message", data);
     }
   });
 
+  /* -------- DISCONNECT -------- */
   socket.on("disconnect", () => {
     console.log("Disconnected:", socket.id);
-    if (waitingUser === socket) waitingUser = null;
+
+    leaveRoom(socket);
+
+    if (waitingUser === socket) {
+      waitingUser = null;
+    }
   });
 });
+
+/* ================= SERVER ================= */
 
 const PORT = process.env.PORT || 3000;
 
